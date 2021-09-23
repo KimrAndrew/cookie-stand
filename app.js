de72fc109419465050b7e53ffa486088f9faf0a9
KimrAndrew <andrewrk1997@gmail.com>
2021-09-23 16:52:18 -0700

    app.js now able to create and render new locations based off of information received from html form

--- app.js
+++ app.js
@@ -6,9 +6,13 @@ generateSale = function(location) {
     //generate customers for the hour
     //generate random number of customers between the minimum and maximum limits
     let customers = Math.floor(Math.random() * (location.maxCust - location.minCust + 1)) + location.minCust;
+    //console.log(customers + " customers");
     //console.log(location.maxCust);
     //generate cookie sales from customer attendance and average number of cookies sold
     let sale = Math.floor(customers * location.avgSale);
+    //console.log(typeof(location.minCust));
+    //console.log(location.minCust + "min cust");
+    //console.log(location.maxCust + "max cust");
     //console.log(sale);
     return sale;
 },
@@ -23,13 +27,14 @@ Shop = function(name,minCust,maxCust,avgSale) {
     this.salesByHour = [];
     this.generateSales = function() {
         //console.log('generating sales');
-        for(let i = 0; i < this.hours.length; i++) {
+        for(let i = 0; i < Shop.prototype.hours.length; i++) {
 
         let sale = generateSale(this);
         //console.log(sale);
         this.salesByHour.push(sale);
         }
     }
+    Locations.push(this);
 };
 
 Shop.prototype.numLocations = Locations.length;
@@ -59,7 +64,7 @@ renderTableHead = function() {
 //Render the table body on the webpage
 Shop.prototype.render = function() {
     let table = document.getElementById('Sales');
-    let tbody = document.createElement('tbody');
+    //let tbody = document.createElement('tbody');
     let tr = document.createElement('tr');
     let th = document.createElement('th');
     th.innerHTML = this.name;
@@ -84,16 +89,16 @@ Shop.prototype.render = function() {
 
 let Seattle = new Shop('Seattle',23,65,6.3);
 Seattle.generateSales();
-Locations.push(Seattle);
+//Locations.push(Seattle);
 
 
 let Tokyo = new Shop('Tokyo',3,24,1.2);
 Tokyo.generateSales();
-Locations.push(Tokyo);
+//Locations.push(Tokyo);
 
 let Dubai = new Shop('Dubai',11,38,3.7);
 Dubai.generateSales();
-Locations.push(Dubai);
+//Locations.push(Dubai);
 
 let Paris = new Shop('Paris',20,38,3.2);
 Paris.generateSales();
@@ -101,7 +106,7 @@ Locations.push(Paris);
 
 let Lima = new Shop('Lima',2,16,4.7);
 Lima.generateSales();
-Locations.push(Lima);
+//Locations.push(Lima);
 
 //Calculates the total cookies sold between locations for each hour of operation
 let calcHourlyTotal = function(locationArr) {
@@ -147,6 +152,22 @@ let renderTableFooter = function() {
 calcHourlyTotal(Locations);
 //console.log(hourlyTotals);
 
+let createNewLocation = function(formSubmission) {
+    formSubmission.preventDefault();
+    let locationName = formSubmission.target.locationName.value;
+
+    //WHY DO THESE RETURN STRINGS WHEN TYPE IS SET TO NUMBER?????
+    let minCust = parseInt(formSubmission.target.minCust.value);
+    let maxCust = parseInt(formSubmission.target.maxCust.value);
+    let avgSale = parseFloat(formSubmission.target.avgSale.value);
+    let newShop = new Shop(locationName,minCust,maxCust,avgSale);
+    newShop.generateSales();
+    console.log(newShop);
+    newShop.render();
+}
+
+let locationForm = document.getElementById('location-form');
+locationForm.addEventListener('submit', createNewLocation);
 renderTableHead();
 
 Seattle.render();
@@ -156,3 +177,5 @@ Paris.render();
 Lima.render();
 
 renderTableFooter();
+console.log("done");
+console.log(Locations);
